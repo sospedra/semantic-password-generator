a738dd21e1e6badc202a17491dd5b01be486680c
sospedra <sospedra.r@gmail.com>
2017-06-18 16:17:56 +0200

    feat: Add default values

--- src/generator.js
+++ src/generator.js
@@ -1,4 +1,7 @@
 const random = require('lodash.random')
+const isNumber = require('lodash.isnumber')
+const identity = require('lodash.identity')
+
 const password = require('./password')
 const transformNormalize = require('./transform/normalize')
 const transformLength = require('./transform/length')
@@ -7,13 +10,37 @@ const transformCase = require('./transform/case')
 const transformRandom = require('./transform/random')
 const transformSymbols = require('./transform/symbols')
 
-const LENGTH_DEFAULT = 24
+/**
+ * Standarized options to set the base sentence and pipe transforms
+ * @typedef {Object} Options
+ * @property {Bool} case     [false]  - apply case transform
+ * @property {Number} length [24]     - min and orientative sentence length
+ * @property {Bool} leet     [false]  - apply leet transform
+ * @property {Bool} random   [false]  - apply random transform
+ * @property {Bool} symbols  [false]  - apply symbols transform
+ */
+
+/**
+ * Transform incoming args into standarized options
+ * @param  {Number|Object} args
+ * @return {@type Options}
+ */
+const sanitizeOptions = (args) => {
+  return Object.assign({}, {
+    case: false,
+    length: isNumber(args) || 24,
+    leet: false,
+    random: false,
+    symbols: false
+  }, args)
+}
 
 module.exports = (sentences) => {
   const maxLength = password.getMaxLength(sentences)
 
-  return function generator (inputLength = LENGTH_DEFAULT) {
-    const length = password.getLength(inputLength, maxLength)
+  return function generator (args) {
+    const options = sanitizeOptions(args)
+    const length = password.getLength(options.length, maxLength)
     const data = sentences.filter((x) => x.length >= length)
     const base = data[random(data.length - 1)]
 
@@ -22,10 +49,12 @@ module.exports = (sentences) => {
     return [
       transformNormalize(),
       transformLength(length),
-      transformLeet(),
-      transformCase(),
-      transformRandom(),
-      transformSymbols()
-    ].reduce((memo, fn) => fn(memo), base)
+      options.leet && transformLeet(),
+      options.case && transformCase(),
+      options.random && transformRandom(),
+      transformSymbols(options.symbols)
+    ]
+      .map((fn) => fn ? fn : identity)
+      .reduce((memo, fn) => fn(memo), base)
   }
 }
--- src/transform/symbols.js
+++ src/transform/symbols.js
@@ -1,7 +1,8 @@
 const random = require('lodash.random')
 const shall = require('../shall')
 
-const SYMBOLS = '@#$%{}[]()/~,;:.><'.split('')
+const DEFAULT_SYMBOL = '.'
+const SYMBOLS = `@#$%{}[]()/~,;:><${DEFAULT_SYMBOL}`.split('')
 const EMPTY_CHAR = ''
 const RX_WHITE_SPACE = / /g
 
@@ -9,13 +10,16 @@ const RX_WHITE_SPACE = / /g
  * Replace white spaces either by a random symbol or an empty char (remove).
  * Note that this transformer gets rid of the white spaces. Hence has to be
  * the last one always.
+ * If it's disabled always return the DEFAULT_SYMBOL
  *
  * @param  {String} base
  * @return {String}
  */
-module.exports = () => {
+module.exports = (isEnabled) => {
   return function transformSymbols (base) {
     return base.replace(RX_WHITE_SPACE, () => {
+      if (!isEnabled) return DEFAULT_SYMBOL
+
       return shall()
         ? SYMBOLS[random(SYMBOLS.length - 1)]
         : EMPTY_CHAR
